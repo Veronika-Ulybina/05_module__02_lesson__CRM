d6524dfe528f17e5e9757b39dd5c3d753ce0410d
Veronika Ulybina <veronica.ulybina@yandex.ru>
2023-12-25 18:29:03 +0500

    Adds a modifier class, receives modal window elements as constants

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 'use strict';
 
-const name = 'navigation system';
-const amount = 7;
-const category = 'home appliances';
-const price = 100;
-const totalAmount = amount * price;
+const modalTitle = document.querySelectorAll('h2');
 
-console.log(name);
-console.log(totalAmount);
+const idBtn = document.querySelector('.identifier-btn');
+
+const idOutput = document.querySelector('.identifier-block__output');
+
+const form = document.querySelector('.form');
+
+const checkbox = document.querySelector('.form-data__checkbox');
+
+const inputCheckbox = document.querySelector('.form-data__input_grey');
+
+const totalPrice = document.querySelector('.total-cost-block output');
