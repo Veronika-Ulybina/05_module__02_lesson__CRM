b1b4115da422c91403cec82cb1ee06745a98a250
Veronika Ulybina <veronica.ulybina@yandex.ru>
2023-12-25 16:25:40 +0500

    init dev

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const name = 'navigation system';
+const amount = 7;
+const category = 'home appliances';
+const price = 100;
+const totalAmount = amount * price;
+
+console.log(name);
+console.log(totalAmount);
